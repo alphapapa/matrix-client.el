4422ecdbc8e99849b7dca22b27f8566d744ad492
Ryan Rix <rrix@uber.com>
2015-10-30 22:42:03 +0000

    Update documentation for project

--- matrix-client.el
+++ matrix-client.el
@@ -26,8 +26,12 @@
 
 ;;; Commentary:
 
-;; `matrix-client' is a chat client and API library for the Matrix.org decentralized
-;; RPC system.
+;; `matrix-client' is a chat client and API library for the Matrix.org decentralized RPC
+;; system. `(package-install 'matrix-client)' and then either deploy your own homeserver or register
+;; an account on the public homeserver https://matrix.org/beta/#/login . After you've done that, M-x
+;; matrix-client will set you up with buffers corresponding to your Matrix rooms. You can join new
+;; ones with /join, leave with /leave or /part, and hook in to the custom functions provided by
+;; =matrix-client=.
 
 ;; Implementation-wise `matrix-client' itself provides most of the core plumbing for
 ;; an interactive Matrix chat client. It uses the Matrix event stream framework
