1f599e6d21dcf0f8dac2bd32df1caa49011b3353
Adam Porter <adam@alphapapa.net>
2017-11-23 05:31:48 -0600

    Update tests

--- test/test-matrix-api-r0.3.0.el
+++ test/test-matrix-api-r0.3.0.el
@@ -6,57 +6,66 @@
 (require 'a)
 (require 'buttercup)
 (require 'f)
+(require 'ht)
 (require 'json)
 (require 'request)
 
 
 (describe "matrix-client"
 
+  ;; Load/update API files
   (let ((load-path (list default-directory
                          (f-parent default-directory)
                          (f-join default-directory "test"))))
-    ;;  (require 'matrix-api-r0.3.0)
-    (load "matrix-api-r0.3.0")
-    (load "password"))
+    (if (featurep 'matrix-api-r0.3.0)
+        ;; Already loaded, e.g. running `buttercup-run-at-point' in a
+        ;; running Emacs instance: `load' the files to get changes.
+        (load "matrix-api-r0.3.0")
+      ;; Probably running from Cask: just `require'.
+      (require 'matrix-api-r0.3.0))
+    ;; Load test account
+    (load "account"))
 
   (before-all
-    (setq session (matrix-session :user "@matrix-client-el-tester:matrix.org"
-                                  :device-id (md5 (concat "matrix-client.el/test.el" (current-time-string) (system-name)))
-                                  :initial-device-display-name (format "matrix-client.el/test.el @ %s (%s)"
-                                                                       (system-name) (format-time-string "%F %T"))))
+    (setq matrix-test-session (matrix-session :user matrix-test-user
+                                              :device-id (md5 (concat "matrix-client.el/test.el" (current-time-string) (system-name)))
+                                              :initial-device-display-name (format "matrix-client.el/test.el @ %s (%s)"
+                                                                                   (system-name) (format-time-string "%F %T"))))
     (setq matrix-synchronous t)
     (defvar matrix-test-joined-rooms nil))
 
   (it "Can log in and get an access_token"
-    (matrix-login session matrix-password)
-    (expect (oref session access-token)
+    (matrix-login matrix-test-session matrix-test-password)
+    (expect (oref matrix-test-session access-token)
             :to-match (rx (1+ alnum))))
 
   (describe "Rooms"
 
+    (before-all
+      (setq matrix-rooms-initial-length (length (oref matrix-test-session rooms))))
+
     (it "Can create a room"
-      (let ((rooms-length (length (oref session rooms))))
-        (matrix-create-room session)
-        ;; Used to forget the room later
-        (push (car (oref session rooms)) matrix-test-joined-rooms)
-        (expect (length (oref session rooms))
-                :to-be-greater-than rooms-length)))
+      (matrix-create-room matrix-test-session)
+      ;; Used to forget the room later
+      (push (car (oref matrix-test-session rooms)) matrix-test-joined-rooms)
+      (expect (length (oref matrix-test-session rooms))
+              :to-be-greater-than matrix-rooms-initial-length))
 
     (describe "Messages"
 
       (it "Can send messages to a room"
         (spy-on #'matrix-send-message-callback :and-call-through)
-        (matrix-send-message (car (oref session rooms)) "Test message.")
+        (matrix-send-message (car (oref matrix-test-session rooms)) "Test message.")
         (expect #'matrix-send-message-callback :to-have-been-called))
 
       (it "Can sync messages from a room"
         ;; TODO: This is more generic and wide-ranging than just rooms
         ;; or messages.  Maybe we need to do two sessions, and sync
         ;; previous messages in the next one.
-        (matrix-sync session)
-        (expect (length (oref (car (oref session rooms)) timeline))
+        (matrix-sync matrix-test-session)
+        (expect (length (oref (car (oref matrix-test-session rooms)) timeline))
                 :to-be-greater-than 0)
-        (expect (pcase-let* (((eieio rooms) session)
+        (expect (pcase-let* (((eieio rooms) matrix-test-session)
                              (room (car rooms))
                              ((eieio timeline) room)
                              (first-event (car timeline)))
@@ -70,18 +79,18 @@
     (xit "Can change room alias")
 
     (it "Can leave a room"
-      (matrix-leave (car (oref session rooms)))
-      (expect (length (oref session rooms))
-              :to-equal 0))
+      (matrix-leave (car (oref matrix-test-session rooms)))
+      (expect (length (oref matrix-test-session rooms))
+              :to-equal matrix-rooms-initial-length))
 
     (it "Can forget a room"
       (matrix-forget (car matrix-test-joined-rooms)))
 
     (xit "Can fetch more messages"
-      (pcase-let* ((room (car (oref session rooms)))
+      (pcase-let* ((room (car (oref matrix-test-session rooms)))
                    ((eieio id) room))
-        (matrix-messages session id))
-      (expect (length (oref (car (oref session rooms)) timeline))
+        (matrix-messages matrix-test-session id))
+      (expect (length (oref (car (oref matrix-test-session rooms)) timeline))
               ;; The default number to fetch at one time is 10, so
               ;; after syncing and fetching more, there should be more
               ;; than 10.
@@ -95,8 +104,4 @@
 
     ;; TODO: Need a second test account that remains in rooms with messages for testing initial sync.
 
-)
-
-
-
-  )
+    ))
