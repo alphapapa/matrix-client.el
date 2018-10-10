0bcb790ceb5993e2fe0c4a57cdac5140ee9de401
Adam Porter <adam@alphapapa.net>
2018-10-10 10:12:35 -0500

    Add: /upload command for images
    Can easily be extended to other filetypes later.
    
    Squashed commit of the following:
    
    commit 96dc95cdc8102f4f28dccfea522bd7b365b493fe
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Wed Oct 10 10:11:07 2018 -0500
    
        Change: Don't advise url-http-create-request
    
        Use cl-letf to override it where needed.
    
    commit d7591f9c1496e23bc2072d113b401d8c20caba59
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Sun Sep 2 00:13:35 2018 -0500
    
        Tidy: upload callback
    
    commit 0de8f93309d10764e5a1bbc2662253435267e140
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Sat Sep 1 23:57:27 2018 -0500
    
        Comment: Add comment about fixing the fix
    
    commit b02190f5b748d4c59b6d38ca95c7e6c799db6434
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Sun Aug 26 23:52:45 2018 -0500
    
        Add: /upload command
    
    commit 48da8059c61b8f63e2f9ad7b0498d1449b6de988
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Sun Aug 26 23:52:20 2018 -0500
    
        Add/Change: Download HTTP paths, then upload
    
        Also convert function to method
    
    commit e1f5eb5754253895cf413ad3e52e3cf23e62846d
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Sun Aug 26 23:50:36 2018 -0500
    
        Fix: Get image type from file header if no extension
    
    commit a1b4c3faa38c426a7804b33b444cd2bb7e461c62
    Author: Adam Porter <adam@alphapapa.net>
    Date:   Thu Aug 23 00:33:09 2018 -0500
    
        WIP: Image upload
    
        Plus everyone's favorite, cowsay!

--- scratch.el
+++ scratch.el
@@ -36,3 +36,20 @@
     (unless buffer
       (setf buffer "BUFFER NAME")))
   (oref room extra))
+
+;;; derp
+
+(cl-defmethod matrix-client-ng-room-command-cowsay ((room matrix-room) input)
+  "Cowsay!"
+  (let* ((s (replace-regexp-in-string (rx bos "/" (1+ (not space)) (1+ space)) "" input))
+         (cow-type (seq-random-elt '("-b" "-d" "-g" "-p" "-s" "-t" "-w" "-y")))
+         (cowsaid (shell-command-to-string (format "cowsay %s %s" cow-type (shell-quote-argument s))))
+         (html (concat "<font color=\"#000\" data-mx-color=\"#000\" data-mx-bg-color=\"#fff\"><pre>\n"
+                       "<strong>" (htmlize-escape-or-link cowsaid) "</strong>"
+                       "</pre></font>")))
+    (matrix-send-message room (concat "Cow: \"" s "\"")
+                         :extra-content (a-list 'formatted_body html
+                                                'format "org.matrix.custom.html"))))
+
+;; (defun cowsay (s)
+;;   (shell-command-to-string (format "cowsay %s" (shell-quote-argument s))))
