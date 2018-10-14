00c711b22656413e7d18ca231cf1d17099a41c25
Adam Porter <adam@alphapapa.net>
2018-10-14 11:58:46 -0500

    Tidy: Comments and docstrings

--- ordered-buffer.el
+++ ordered-buffer.el
@@ -20,7 +20,10 @@
 
 ;;; Commentary:
 
-;;
+;; The result of factoring out some of the buffer-insertion code in matrix-client-room.el.  Could be
+;; useful in other projects.  The basic idea is to insert strings into a buffer, ordered by a text
+;; property (like an integer timestamp).  The strings may "arrive" in any order, but they will
+;; always be inserted at the correct, ordered position.
 
 ;;; Code:
 
@@ -29,14 +32,23 @@
   "Face for headers."
   :group 'ordered-buffer)
 
-(defvar ordered-buffer-header-face 'ordered-buffer-header)
-(defvar ordered-buffer-point-fn #'point-max)
-(defvar ordered-buffer-prefix-fn nil)
+(defvar ordered-buffer-header-face 'ordered-buffer-header
+  "Face applied to headers.")
+(defvar ordered-buffer-point-fn #'point-max
+  "Function called by `ordered-buffer-insert' which returns the position at which to insert a new string.")
+(defvar ordered-buffer-prefix-fn nil
+  "An optional function which is called by `ordered-buffer-insert' before inserting its string.
+Called with point at the insertion position.  May be used to
+insert headers, etc.")
 (defvar ordered-buffer-header-suffix "\n"
   "String inserted after headers.  May be nil.")
 
 (defun ordered-buffer-insert (string &rest properties)
-  "FIXME."
+  "Insert STRING into current buffer at appropriate position.
+The `ordered-buffer-point-fn' function returns the position.  If
+`ordered-buffer-prefix-fn' is non-nil, it is called with point at
+the position before inserting STRING.  PROPERTIES are applied to
+STRING."
   (save-excursion
     (goto-char (funcall ordered-buffer-point-fn))
     (when ordered-buffer-prefix-fn
@@ -44,7 +56,11 @@
     (insert (apply #'propertize string properties))))
 
 (defun ordered-buffer-insert-header (string &rest properties)
-  "FIXME"
+  "Insert header containing STRING at point.
+PROPERTIES are applied to STRING, and the face in
+`ordered-buffer-header-face' is applied to it.  The string
+`ordered-buffer-header-suffix' is appended to the header.  The
+header has the text-property `ordered-buffer-header' set."
   (let* ((visible-header (propertize (concat " " string "\n")
                                      'face ordered-buffer-header-face))
          (whole-header (apply #'propertize (concat "\n" visible-header ordered-buffer-header-suffix)
@@ -54,7 +70,21 @@
     (insert whole-header)))
 
 (cl-defun ordered-buffer-point-fn (&key backward-from forward-from property comparator value)
-  "FIXME"
+  "Return position at which a new string should be inserted, depending on criteria.
+
+One of BACKWARD-FROM or FORWARD-FROM may be set and should be an
+integer position in the buffer or a function which returns a
+position, from which the search starts.
+
+PROPERTY should be a symbol of the text property (which should
+not be a keyword symbol) which is compared with VALUE using
+COMPARATOR.
+
+When the comparison is non-nil, the point at that position is
+returned.  If the search reaches a point after which PROPERTY
+does not change again in the buffer, the point returned depends
+on the search direction: if BACKWARD-FROM, `point-min'; if
+FORWARD-FROM, `point-max'."
   (declare (indent defun))
   (when (and backward-from forward-from)
     (user-error "Only one of `:backward-from' or `:forward-from' may be set"))
