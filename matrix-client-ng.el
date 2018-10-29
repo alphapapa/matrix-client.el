33a0b33852ba54219e1e949d62d2169316d2644d
Jay Kamat <jaygkamat@gmail.com>
2018-10-28 21:34:16 -0700

    Attempt to add back package headers
    not sure if I missed any dependencies, let me know if anyone sees
    anything missing.

--- matrix-client-ng.el
+++ matrix-client-ng.el
@@ -1,4 +1,27 @@
-;;; matrix-client-ng.el --- summary ---  -*- lexical-binding: t; -*-
+;;; matrix-client-ng.el --- A matrix client for Emacs ---  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Adam Poter
+;; Author: Adam Porter <adam@alphapapa.net>
+;; Maintainer: Jay Kamat <jaygkamat@gmail.com>
+;; Created: 28 October 2018
+;; Keywords: web, comm
+;; Homepage: https://github.com/jgkamat/matrix-client-el
+;; Package-Version: 0.1.0
+;; Package-Requires: ((emacs "25.1") (dash "2.13.0") (f "0.17.2") (request "0.2.0") (a "0.1.0") (ov "1.0.6") (s "1.12.0") (tracking "2.9") (esxml "0.3.4"))
+
+;; This file is not part of GNU Emacs.
+;; matrix-client-images.el is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at your
+;; option) any later version.
+;;
+;; matrix-client-images.el is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
