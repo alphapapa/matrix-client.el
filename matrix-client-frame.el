5a1583608ef415f5d30e1e47793698e302d4fa96
Adam Porter <adam@alphapapa.net>
2018-12-25 16:05:16 -0600

    Add: matrix-client-frame-sort-fns

--- matrix-client-frame.el
+++ matrix-client-frame.el
@@ -1,9 +1,51 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'cl-lib)
+(require 'eieio)
+(require 'subr-x)
+
+(require 'a)
 (require 'dash)
 (require 'dash-functional)
 (require 'frame-purpose)
 
 (defvar matrix-client-frame nil
   "The current Matrix Client frame.")
+
+;; TODO: It'd be nice to byte-compile these comparators, because it doesn't seem to happen just by
+;; byte-compiling the file.
+(defcustom matrix-client-frame-sort-fns
+  (list (lambda (a b)
+          (string< (buffer-name a)
+                   (buffer-name b)))
+        (lambda (a b)
+          ;; Sort modified rooms first (i.e. rooms with unseen events).
+          (and (buffer-modified-p a)
+               (not (buffer-modified-p b)))))
+  "How to sort room buffers in the frame sidebar.
+A list of functions that take two room buffers as arguments and
+return non-nil if the first should be sorted before the second."
+  :group 'matrix-client
+  :type `(repeat (choice (const :tag "Room name"
+                                (lambda (a b)
+                                  (string< (buffer-name a)
+                                           (buffer-name b))))
+                         (const :tag "Most recent event in room"
+                                (lambda (a b)
+                                  (> (matrix-client-buffer-latest-event-ts a)
+                                     (matrix-client-buffer-latest-event-ts b))))
+                         (const :tag "Unseen events"
+                                (lambda (a b)
+                                  ;; Sort modified rooms first (i.e. rooms with unseen events).
+                                  (and (buffer-modified-p a)
+                                       (not (buffer-modified-p b))))))))
+
+;; This function can stay here for now since it's only used here.
+(defun matrix-client-buffer-latest-event-ts (buffer)
+  "Return timestamp of latest event in BUFFER's room."
+  (when-let* ((room (buffer-local-value 'matrix-client-room buffer))
+              (last-event (car (oref* room timeline))))
+    (a-get* last-event 'origin_server_ts)))
 
 ;;;###autoload
 (defun matrix-client-frame (&optional side)
@@ -24,17 +66,7 @@ automatically."
          :title "Matrix"
          :icon-type (expand-file-name "logo.png" (file-name-directory (locate-library "matrix-client-frame")))
          :sidebar side
-         :buffer-sort-fns (list (-on #'string< #'buffer-name)
-                                (-on #'> (lambda (buffer)
-                                           ;; Sort by most recent event in room.
-                                           (or (when-let* ((room (buffer-local-value 'matrix-client-room buffer))
-                                                           (last-event (car (oref* room timeline))))
-                                                 (a-get* last-event 'origin_server_ts))
-                                               0)))
-                                (lambda (a b)
-                                  ;; Sort modified rooms first (i.e. rooms with unseen events).
-                                  (and (buffer-modified-p a)
-                                       (not (buffer-modified-p b)))))
+         :buffer-sort-fns matrix-client-frame-sort-fns
          :sidebar-buffers-fn (lambda ()
                                (cl-loop for session in matrix-client-sessions
                                         append (cl-loop for room in (oref* session rooms)
