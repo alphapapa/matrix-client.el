9db289099cc13b6cd00057c03045fe9af98113bf
ahmed saad <saad@saadnpq.com>
2019-04-29 21:53:15 +0200

    added switch-buffer-functions as a dep.

--- matrix-client.el
+++ matrix-client.el
@@ -12,7 +12,7 @@
 ;; Homepage: https://github.com/alphapapa/matrix-client.el
 
 ;; Package-Version: 0.2.0-pre
-;; Package-Requires: ((emacs "25.1") (anaphora "1.0.0") (dash "2.13.0") (dash-functional "1.2.0") (f "0.17.2") (request "0.2.0") (a "0.1.0") (ov "1.0.6") (s "1.12.0") (tracking "2.9") (esxml "0.3.4") (ht "2.2") (rainbow-identifiers "0.2.2") (frame-purpose "1.2"))
+;; Package-Requires: ((emacs "25.1") (anaphora "1.0.0") (dash "2.13.0") (dash-functional "1.2.0") (f "0.17.2") (request "0.2.0") (a "0.1.0") (ov "1.0.6") (s "1.12.0") (tracking "2.9") (esxml "0.3.4") (ht "2.2") (rainbow-identifiers "0.2.2") (frame-purpose "1.2") (switch-buffer-functions "0.0.1"))
 
 ;; This file is not part of GNU Emacs.
 
